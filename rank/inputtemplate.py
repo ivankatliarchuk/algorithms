e053c068eae37136b782cefd40f296d9ca8d6b1c
Ivan Katliarchuk <tanzwud.work@gmail.com>
2017-02-06 23:20:31 +0000

    extra input lines

--- rank/inputtemplate.py
+++ rank/inputtemplate.py
@@ -5,5 +5,9 @@ nums = list(map(int, input().split()))
 data = sys.stdin.readlines()[0].split()
 
 
+a,b,c,d,e = input().strip().split(' ')
+a,b,c,d,e = [int(a),int(b),int(c),int(d),int(e)]
+
+
 for line in sys.stdin:
     print(line)
